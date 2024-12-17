4eb81ec4deff42d9576a3bd9042d699072b339a4
ioan_stefan.greere <ioan_stefan.greere@stud.acs.upb.ro>
2024-12-18 01:17:26 +0200

    Added Snake class with movement and rendering functionality

--- main.py
+++ main.py
@@ -16,15 +16,54 @@ WHITE = (255, 255, 255)
 # Clock
 CLOCK = pygame.time.Clock()
 
+class Snake:
+    def __init__(self):
+        self.x = SCREEN_WIDTH // 2
+        self.y = SCREEN_HEIGHT // 2
+        self.size = 40
+        self.change_x = 0
+        self.change_y = 0
+        self.body = [[self.x, self.y]]
+        self.length = 1
+
+    def move(self):
+        self.x += self.change_x
+        self.y += self.change_y
+        self.body.append([self.x, self.y])
+        if len(self.body) > self.length:
+            del self.body[0]
+
+    def draw(self):
+        for segment in self.body:
+            pygame.draw.rect(SCREEN, BLACK, (segment[0], segment[1], self.size, self.size))
+
 def main():
     running = True
+    snake = Snake()
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        SCREEN.fill(WHITE)  # Clear the screen
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    snake.change_x = -snake.size
+                    snake.change_y = 0
+                elif event.key == pygame.K_RIGHT:
+                    snake.change_x = snake.size
+                    snake.change_y = 0
+                elif event.key == pygame.K_UP:
+                    snake.change_y = -snake.size
+                    snake.change_x = 0
+                elif event.key == pygame.K_DOWN:
+                    snake.change_y = snake.size
+                    snake.change_x = 0
+
+        snake.move()
+        SCREEN.fill(WHITE)
+        snake.draw()
         pygame.display.update()
-        CLOCK.tick(30)  # Set the frame rate
+        CLOCK.tick(10)
 
     pygame.quit()
     sys.exit()
